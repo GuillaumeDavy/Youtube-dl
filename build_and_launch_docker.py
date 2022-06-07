2df2ed39745b7e874b5c2650f9fd7afc88e10f37
Guillaume DAVY <guillaume.davy@lunatech.fr>
2022-06-07 16:27:24 +0200

    docker

--- build_and_launch_docker.py
+++ build_and_launch_docker.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 
 subprocess.call("docker build -t youtube-dl-api .", shell=True)
-subprocess.call("docker run --name youtube-dl-api -v ${HOME}/Desktop/videos:/video_storage -p 5001:5001 youtube-dl-api", shell=True)
+subprocess.call("docker run --name youtube-dl-api -v ${HOME}/Desktop/videos:/app/video_storage -p 5001:5001 youtube-dl-api", shell=True)
 
